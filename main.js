7d685b38fc38f47df2dfc3461a6f670ddf5eccfb
1wunat2 <nwu1000@gmail.com>
2023-04-27 00:11:59 -0400

    Added led and messages

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
+//Will ping the website to check if there is a connection
 
-setInterval(isOnline, 30000); //Every 30s
+//var website = 'google.com';
+const b = require('bonescript');
+
+function OnOff(){
+    if(window.navigator.onLine){
+        console.log('ON');
+        ledOn("USR0");
+        ledOff("USR1");
+    } else{
+        console.log('OFF');
+        ledOn("USR1");
+        ledOff("USR0");
+    }
+}
+
+setInterval(OnOff(), 1000);
 
 
-function isOnline(){
-	if(!navigator.onLine) {
-		//reset
-	}
+function ledOn(usr){
+    b.pinMode(usr, b.HIGH);
+}
+function ledOff(usr){
+    b.pinMode(usr, b.LOW);
 }
